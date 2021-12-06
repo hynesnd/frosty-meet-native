1595e6b1c9d46bc9905614263cd79d4821dfd257
hynesnd <hynesnd@gmail.com>
2021-12-06 15:59:44 +0000

    Adds skeleton Home component

--- App.js
+++ App.js
@@ -4,14 +4,16 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Login } from "./components/Login";
+import { Home } from "./components/Home";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator screenOptions={{ headerShown: true }}>
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
