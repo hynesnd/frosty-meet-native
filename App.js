e25f4846e232a8a593f596c6abcbef0bb59e0172
hynesnd <hynesnd@gmail.com>
2021-12-06 15:56:09 +0000

    adds react navigation to app.js

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Login } from "./components/Login";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View>
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-      </View>
-      <Login />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
