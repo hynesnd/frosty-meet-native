a569599f282144b86249206790e75205b24fa2e5
cynthiaswill <yzhang4@msn.com>
2021-12-07 10:53:03 +0000

    installed more dependencies for nav menu

---
+++ constants/DrawerItems.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    name: "Home",
+    iconType: "Material",
+    iconName: "face-profile",
+  },
+  {
+    name: "Login",
+    iconType: "Feather",
+    iconName: "settings",
+  },
+];
---
+++ screens/Login.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, Button, Pressable } from "react-native";
+
+export const Login = ({ navigation }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  console.log(password);
+
+  return (
+    <View style={styles.pageContainer}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Welcome to FrostyMeet</Text>
+      </View>
+      <View style={styles.formContainer}>
+        <TextInput
+          style={styles.input}
+          value={username}
+          onChangeText={setUsername}
+          placeholder="Username:"
+        />
+
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+          placeholder="Password:"
+          secureTextEntry={true}
+        />
+
+        <Pressable style={styles.button} onPress={() => navigation.navigate("Home")}>
+          <Text style={styles.buttonText}>Login</Text>
+        </Pressable>
+
+        <Pressable style={styles.button} onPress={() => navigation.navigate("Home")}>
+          <Text style={styles.buttonText}>Sign up</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    marginTop: 16,
+    marginBottom: 20,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    backgroundColor: "#61dafb",
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  titleContainer: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#eaeaea",
+    justifyContent: "center",
+  },
+  input: {
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    borderWidth: 1,
+    borderColor: "red",
+    padding: 3,
+    fontSize: 18,
+    borderRadius: 4,
+  },
+
+  formContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+
+  pageContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "navy",
+    margin: 10,
+  },
+
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+  },
+});
